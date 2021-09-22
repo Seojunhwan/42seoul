0470a43c24f6e9551d5f34b46b86672f16bf474a
SeoJunhwan <seojunhwan@kakao.com>
2021-09-22 16:25:00 +0900

    edit c05

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: junseo <junseo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 10:12:47 by junseo            #+#    #+#             */
-/*   Updated: 2021/09/20 10:25:19 by junseo           ###   ########.fr       */
+/*   Updated: 2021/09/21 19:38:23 by junseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_iterative_factorial(int nb)
 		return (1);
 	i = 1;
 	result = 1;
-	while (1 <= nb)
+	while (i <= nb)
 	{
 		result = result * i;
 		i++;
--- c05/ex07/ft_find_next_prime.c
+++ c05/ex07/ft_find_next_prime.c
@@ -6,14 +6,48 @@
 /*   By: junseo <junseo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 18:34:52 by junseo            #+#    #+#             */
-/*   Updated: 2021/09/20 23:59:56 by junseo           ###   ########.fr       */
+/*   Updated: 2021/09/21 20:16:10 by junseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_sqrt(int nb)
+{
+	long long	i;
+
+	i = 1;
+	while (i * i < nb)
+		i++;
+	return (i);
+}
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+		return (0);
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	i = 3;
+	while (i <= ft_sqrt(nb))
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	ft_find_next_prime(int nb)
 {
-	i = nb;
-
-	
-
+	if (nb <= 2)
+		return (2);
+	while (nb)
+	{
+		if (ft_is_prime(nb))
+			return (nb);
+	}
+	return (0);
 }
