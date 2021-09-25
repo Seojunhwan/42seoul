46fd36fcb172affeae3e08d11efeaa8ce60a3bee
Seo Junhwan <junseo@student.42seoul.kr>
2021-09-25 18:07:10 +0900

    good

---
+++ c08_0925/ex04/ft_strs_to_tab.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junseo <junseo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/22 14:48:10 by junseo            #+#    #+#             */
+/*   Updated: 2021/09/23 15:21:05 by junseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_strcpy(char *dest, char *src)
+{
+	while (*src)
+		*(dest++) = *(src++);
+	*dest = 0;
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str	*result;
+	int			i;
+
+	result = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		result[i].size = ft_strlen(av[i]);
+		result[i].str = (char *)malloc(result[i].size + 1);
+		result[i].copy = (char *)malloc(result[i].size + 1);
+		ft_strcpy(result[i].str, av[i]);
+		ft_strcpy(result[i].copy, av[i]);
+		i++;
+	}
+	result[i].str = 0;
+	return (result);
+}
---
+++ c08_0925/ex05/ft_show_tab.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junseo <junseo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/22 15:15:58 by junseo            #+#    #+#             */
+/*   Updated: 2021/09/22 16:20:48 by junseo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	print_size(int nb)
+{
+	char	num;
+
+	if (nb == 0)
+		return ;
+	print_size(nb / 10);
+	num = nb % 10 + '0';
+	write(1, &num, 1);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str)
+	{
+		write(1, par[i].str, par[i].size);
+		write(1, "\n", 1);
+		if (par[i].size == 0)
+			write(1, "0", 1);
+		else
+			print_size(par[i].size);
+		write(1, "\n", 1);
+		write(1, par[i].copy, par[i].size);
+		write(1, "\n", 1);
+		i++;
+	}
+}
