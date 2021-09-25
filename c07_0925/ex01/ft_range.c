915683d4336335208ec4664170e4f4e793a73f42
SeoJunhwan <seojunhwan@kakao.com>
2021-09-25 17:55:45 +0900

    good

--- c07_0925/ex01/ft_range.c
+++ c07_0925/ex01/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: junseo <junseo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 01:22:00 by junseo            #+#    #+#             */
-/*   Updated: 2021/09/23 16:29:36 by junseo           ###   ########.fr       */
+/*   Updated: 2021/09/25 11:42:13 by junseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	*ft_range(int min, int max)
 	int	i;
 
 	i = 0;
-	len =  max - min;
+	len = max - min;
 	if (min >= max)
 		return (0);
 	result = (int *)malloc(sizeof(int) * len);
