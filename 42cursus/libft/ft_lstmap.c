b98d79fd2ee416b0d83c89beff688d079c00bed1
SeoJunhwan <seojunhwan@kakao.com>
2021-11-23 20:07:16 +0900

    lstmap 수정

--- 42cursus/libft/ft_lstmap.c
+++ 42cursus/libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: junseo <junseo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 21:04:45 by junseo            #+#    #+#             */
-/*   Updated: 2021/11/22 22:47:40 by junseo           ###   ########.fr       */
+/*   Updated: 2021/11/23 20:07:07 by junseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,27 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*head;
+	t_list	*current_node;
 
 	if (!lst)
 		return (NULL);
 	head = ft_lstnew(f(lst->content));
-	if (ft_lstlast(head) == NULL)
+	if (head == NULL)
 	{
 		ft_lstclear(&head, del);
 		return (NULL);
 	}
+	current_node = head;
 	lst = lst->next;
 	while (lst)
 	{
-		ft_lstadd_back(&head, ft_lstnew(f(lst->content)));
-		if (ft_lstlast(head) == NULL)
+		current_node->next = ft_lstnew(f(lst->content));
+		if (current_node->next == NULL)
 		{
 			ft_lstclear(&head, del);
 			return (NULL);
 		}
+		current_node = current_node->next;
 		lst = lst->next;
 	}
 	return (head);
